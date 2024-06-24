018ff5536b086bec249cb04e9dd00992ca2c8033
echo <hello@kevinjamesneal.us>
2024-06-24 17:09:19 -0400

    first

---
+++ pages/index.tsx
@@ -0,0 +1,279 @@
+import {
+  PublicKey,
+  publicKey,
+  Umi,
+} from "@metaplex-foundation/umi";
+import { DigitalAssetWithToken, JsonMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import dynamic from "next/dynamic";
+import { Dispatch, SetStateAction, useEffect, useMemo, useState } from "react";
+import { useUmi } from "../utils/useUmi";
+import { fetchCandyMachine, safeFetchCandyGuard, CandyGuard, CandyMachine, AccountVersion } from "@metaplex-foundation/mpl-candy-machine"
+import styles from "../styles/Home.module.css";
+import { guardChecker } from "../utils/checkAllowed";
+import { Center, Card, CardHeader, CardBody, StackDivider, Heading, Stack, useToast, Text, Skeleton, useDisclosure, Button, Modal, ModalBody, ModalCloseButton, ModalContent, Image, ModalHeader, ModalOverlay, Box, Divider, VStack, Flex } from '@chakra-ui/react';
+import { ButtonList } from "../components/mintButton";
+import { GuardReturn } from "../utils/checkerHelper";
+import { ShowNft } from "../components/showNft";
+import { InitializeModal } from "../components/initializeModal";
+import { image, headerText } from "../settings";
+import { useSolanaTime } from "@/utils/SolanaTimeContext";
+
+const WalletMultiButtonDynamic = dynamic(
+  async () =>
+    (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+  { ssr: false }
+);
+
+const useCandyMachine = (
+  umi: Umi,
+  candyMachineId: string,
+  checkEligibility: boolean,
+  setCheckEligibility: Dispatch<SetStateAction<boolean>>,
+  firstRun: boolean,
+  setfirstRun: Dispatch<SetStateAction<boolean>>
+) => {
+  const [candyMachine, setCandyMachine] = useState<CandyMachine>();
+  const [candyGuard, setCandyGuard] = useState<CandyGuard>();
+  const toast = useToast();
+
+
+  useEffect(() => {
+    (async () => {
+      if (checkEligibility) {
+        if (!candyMachineId) {
+          console.error("No candy machine in .env!");
+          if (!toast.isActive("no-cm")) {
+            toast({
+              id: "no-cm",
+              title: "No candy machine in .env!",
+              description: "Add your candy machine address to the .env file!",
+              status: "error",
+              duration: 999999,
+              isClosable: true,
+            });
+          }
+          return;
+        }
+
+        let candyMachine;
+        try {
+          candyMachine = await fetchCandyMachine(umi, publicKey(candyMachineId));
+          //verify CM Version
+          if (candyMachine.version != AccountVersion.V2){
+            toast({
+              id: "wrong-account-version",
+              title: "Wrong candy machine account version!",
+              description: "Please use latest sugar to create your candy machine. Need Account Version 2!",
+              status: "error",
+              duration: 999999,
+              isClosable: true,
+            });
+            return;
+          }
+        } catch (e) {
+          console.error(e);
+          toast({
+            id: "no-cm-found",
+            title: "The CM from .env is invalid",
+            description: "Are you using the correct environment?",
+            status: "error",
+            duration: 999999,
+            isClosable: true,
+          });
+        }
+        setCandyMachine(candyMachine);
+        if (!candyMachine) {
+          return;
+        }
+        let candyGuard;
+        try {
+          candyGuard = await safeFetchCandyGuard(umi, candyMachine.mintAuthority);
+        } catch (e) {
+          console.error(e);
+          toast({
+            id: "no-guard-found",
+            title: "No Candy Guard found!",
+            description: "Do you have one assigned?",
+            status: "error",
+            duration: 999999,
+            isClosable: true,
+          });
+        }
+        if (!candyGuard) {
+          return;
+        }
+        setCandyGuard(candyGuard);
+        if (firstRun){
+          setfirstRun(false)
+        }
+      }
+    })();
+  }, [umi, checkEligibility]);
+
+  return { candyMachine, candyGuard };
+
+
+};
+
+
+export default function Home() {
+  const umi = useUmi();
+  const solanaTime = useSolanaTime();
+  const toast = useToast();
+  const { isOpen: isShowNftOpen, onOpen: onShowNftOpen, onClose: onShowNftClose } = useDisclosure();
+  const { isOpen: isInitializerOpen, onOpen: onInitializerOpen, onClose: onInitializerClose } = useDisclosure();
+  const [mintsCreated, setMintsCreated] = useState<{ mint: PublicKey, offChainMetadata: JsonMetadata | undefined }[] | undefined>();
+  const [isAllowed, setIsAllowed] = useState<boolean>(false);
+  const [loading, setLoading] = useState(true);
+  const [ownedTokens, setOwnedTokens] = useState<DigitalAssetWithToken[]>();
+  const [guards, setGuards] = useState<GuardReturn[]>([
+    { label: "startDefault", allowed: false, maxAmount: 0 },
+  ]);
+  const [firstRun, setFirstRun] = useState(true);
+  const [checkEligibility, setCheckEligibility] = useState<boolean>(true);
+
+
+  if (!process.env.NEXT_PUBLIC_CANDY_MACHINE_ID) {
+    console.error("No candy machine in .env!")
+    if (!toast.isActive('no-cm')) {
+      toast({
+        id: 'no-cm',
+        title: 'No candy machine in .env!',
+        description: "Add your candy machine address to the .env file!",
+        status: 'error',
+        duration: 999999,
+        isClosable: true,
+      })
+    }
+  }
+  const candyMachineId: PublicKey = useMemo(() => {
+    if (process.env.NEXT_PUBLIC_CANDY_MACHINE_ID) {
+      return publicKey(process.env.NEXT_PUBLIC_CANDY_MACHINE_ID);
+    } else {
+      console.error(`NO CANDY MACHINE IN .env FILE DEFINED!`);
+      toast({
+        id: 'no-cm',
+        title: 'No candy machine in .env!',
+        description: "Add your candy machine address to the .env file!",
+        status: 'error',
+        duration: 999999,
+        isClosable: true,
+      })
+      return publicKey("11111111111111111111111111111111");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  const { candyMachine, candyGuard } = useCandyMachine(umi, candyMachineId, checkEligibility, setCheckEligibility, firstRun, setFirstRun);
+
+  useEffect(() => {
+    const checkEligibilityFunc = async () => {
+      if (!candyMachine || !candyGuard || !checkEligibility || isShowNftOpen) {
+        return;
+      }
+      setFirstRun(false);
+      
+      const { guardReturn, ownedTokens } = await guardChecker(
+        umi, candyGuard, candyMachine, solanaTime
+      );
+
+      setOwnedTokens(ownedTokens);
+      setGuards(guardReturn);
+      setIsAllowed(false);
+
+      let allowed = false;
+      for (const guard of guardReturn) {
+        if (guard.allowed) {
+          allowed = true;
+          break;
+        }
+      }
+
+      setIsAllowed(allowed);
+      setLoading(false);
+    };
+
+    checkEligibilityFunc();
+    // On purpose: not check for candyMachine, candyGuard, solanaTime
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [umi, checkEligibility, firstRun]);
+
+  const PageContent = () => {
+    return (
+      <Box bg="black" minHeight="100vh" color="white" p={8}>
+        <Flex justifyContent="space-between" alignItems="center" mb={8}>
+          <VStack align="flex-start" spacing={1}>
+            <Heading size="2xl" fontWeight="normal">{headerText.toLowerCase()}</Heading>
+            {!loading && (
+              <Text fontSize="lg">
+                {Number(candyMachine?.itemsRedeemed)}/{Number(candyMachine?.data.itemsAvailable)} MINTED
+              </Text>
+            )}
+          </VStack>
+          <WalletMultiButtonDynamic />
+        </Flex>
+        
+        <Flex direction={["column", "row"]} alignItems="flex-start" justifyContent="space-between" maxWidth="1200px" margin="0 auto">
+          <Box width={["100%", "50%"]} mb={[8, 0]}>
+            <Image src={image} alt={headerText} width="100%" />
+          </Box>
+          
+          <Box width={["100%", "40%"]}>
+            {loading ? (
+              <VStack spacing={8} align="stretch">
+                <Skeleton height="100px" />
+                <Skeleton height="100px" />
+              </VStack>
+            ) : (
+              <ButtonList
+                guardList={guards}
+                candyMachine={candyMachine}
+                candyGuard={candyGuard}
+                umi={umi}
+                ownedTokens={ownedTokens}
+                setGuardList={setGuards}
+                mintsCreated={mintsCreated}
+                setMintsCreated={setMintsCreated}
+                onOpen={onShowNftOpen}
+                setCheckEligibility={setCheckEligibility}
+              />
+            )}
+          </Box>
+        </Flex>
+
+        {umi.identity.publicKey === candyMachine?.authority && candyMachine && (
+          <Button onClick={onInitializerOpen} mt={4}>Initialize Everything!</Button>
+        )}
+
+        <Modal isOpen={isShowNftOpen} onClose={onShowNftClose}>
+          <ModalOverlay />
+          <ModalContent bg="gray.800" color="white">
+            <ModalHeader>Your minted NFT:</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <ShowNft nfts={mintsCreated} />
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+
+        <Modal isOpen={isInitializerOpen} onClose={onInitializerClose}>
+  <ModalOverlay />
+  <ModalContent bg="gray.800" color="white" maxW="600px">
+    <ModalHeader>Initializer</ModalHeader>
+    <ModalCloseButton />
+    <ModalBody>
+      {candyMachine && (
+        <InitializeModal umi={umi} candyMachine={candyMachine} candyGuard={candyGuard} />
+      )}
+    </ModalBody>
+  </ModalContent>
+</Modal>
+      </Box>
+    );
+  };
+
+  return (
+    <main className={styles.main}>
+      <PageContent />
+    </main>
+  );
+}
---
+++ settings.tsx
@@ -0,0 +1,12 @@
+// Purpose: Contains texts that will be used in the UI when rendering the buttons
+// label has to be the exact same as in your candy machine config
+export const mintText = [
+  { label: "SOL", mintText: "0.18 SOL", buttonLabel: "MINT WITH SOL", header: "SOL MINT" },
+  { label: "AURA", mintText: "100K $AURA", buttonLabel: "MINT WITH AURA", header: "AURA BURN MINT" }
+];
+
+//header image in the ui. replace with your own
+export const image = "https://raw.githubusercontent.com/0xech0/minting-site/main/_0015_1.png";
+
+//website title
+export const headerText = "MILADY AURA THREE";
